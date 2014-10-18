d451e1fa9e65762f6c1757f4c161e7542ca90cfb
Matt Davis <matt.davis129@gmail.com>
2014-10-17 21:39:20 -0700

    finishes up rps and tweaks tic tac toe styles

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -48,15 +48,15 @@ class RpsController < ApplicationController
 
   	when %w{ paper rock }, %w{ scissors paper }, %w{ rock scissors }
   		@score[0] += 1
-			@result = "PLAYER WINS"
+			@result = "You win!"
 
 		when %w{ rock paper }, %w{ paper scissors }, %w{ scissors rock }
 			@score[1] += 1
-			@result = "COMP WINS"
+			@result = "Bummer, you lost."
 			
 		else
 			# it's a tie
-			@result = "TIE"
+			@result = "It's a tie!"
 		end
 	end
 
